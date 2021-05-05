8c3e5bb2193a773d76236a26f3b68b24d5f35b9b
Jon Natkins <jon.natkins@fishtownanalytics.com>
2021-05-05 15:46:41 +0000

    Add lifetime value to customers model

--- models/customers.sql
+++ models/customers.sql
@@ -16,16 +16,24 @@ orders as (
 
 ),
 
-customer_orders as (
+payments as (
+
+    select * from {{ ref('stg_payments') }} where status <> 'fail'
+
+),
+
+customer_order_payments as (
 
     select
         customer_id,
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(orders.order_id) as number_of_orders,
+        sum(coalesce(amount,0)) as lifetime_value
 
     from orders
+    left join payments on (orders.order_id = payments.order_id)
 
     group by 1
 
@@ -38,13 +46,14 @@ final as (
         customers.customer_id,
         customers.first_name,
         customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        cop.first_order_date,
+        cop.most_recent_order_date,
+        coalesce(cop.number_of_orders, 0) as number_of_orders,
+        coalesce(cop.lifetime_value, 0) as lifetime_value
 
     from customers
 
-    left join customer_orders using (customer_id)
+    left join customer_order_payments as cop using (customer_id)
 
 )
 
--- models/orders.sql
+++ models/orders.sql
@@ -1,9 +1,5 @@
 with
 
-customers as (
-    select * from {{ ref('stg_customers') }}
-),
-
 orders as (
     select * from {{ ref('stg_orders') }}
 ),
@@ -13,9 +9,8 @@ payments as (
 )
 
 select 
-  customers.customer_id as customer_id,
+  orders.customer_id as customer_id,
   orders.order_id as order_id,
   payments.amount as amount
-from customers 
-  join orders on (customers.customer_id = orders.customer_id)
+from orders
   join payments on (orders.order_id = payments.order_id)
